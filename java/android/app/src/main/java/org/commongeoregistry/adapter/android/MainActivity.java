f96decfebcd6923cf04457cab776725059150687
Ephraim Kigamba <ekigamba@ona.io>
2019-01-11 18:32:30 +0300

    Add nexus repository url
    - Add cgr-adapter for android
    - Import the right R reference in MainActivity

--- java/android/app/src/main/java/org/commongeoregistry/adapter/android/MainActivity.java
+++ java/android/app/src/main/java/org/commongeoregistry/adapter/android/MainActivity.java
@@ -3,7 +3,7 @@ package org.commongeoregistry.adapter.android;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import org.commongeoregistry.client.android.R;
+
 
 public class MainActivity extends AppCompatActivity {
 
